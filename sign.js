211eb1cfb5f2193d6767cab33a7038f5db8780f0
George Kushnir <admin@n4ru.it>
2018-10-05 07:36:08 -0400

    Add a bunch of CLI stuff.

--- sign.js
+++ sign.js
@@ -1,25 +1,90 @@
 const arkjs = require('arkjs');
 const crypto = arkjs.crypto;
+const fs = require('fs');
 let tx;
 const pass = process.argv.slice(3).join(" ");
 
 const sign = (multi, second) => {
-    tx = require('./tx.json');
-    multi && !tx.signatures && (tx.signatures = []);
-    !tx.type && (tx.type = 0);
-    !multi && !second && (tx.timestamp = arkjs.slots.getTime()) && (tx.signature = crypto.getKeys(pass).sign(crypto.getHash(tx, true, true)).toDER().toString("hex"));
-    !tx.timestamp && (tx.timestamp = arkjs.slots.getTime());
-    multi && (tx.signatures = tx.signatures.concat([crypto.getKeys(pass).sign(crypto.getHash(tx, true, true)).toDER().toString("hex")]));
-    !multi && second && (tx.signSignature = crypto.getKeys(pass).sign(crypto.getHash(tx, true, true)).toDER().toString("hex"));
-    tx.id = crypto.getId(tx);
-    multi && (tx.ready = true);
-    console.log(JSON.stringify(tx, null, 4))
+    tx = fs.existsSync('./tx.json') ? require('./tx.json') : null
+    if (tx) {
+        if (!tx.type) tx.type = 0;
+        if (!(!multi && !second) && !tx.signature) {
+            console.log("Please apply the first signature to this transaction.")
+        } else {
+            let keys = crypto.getKeys(pass);
+            if (!tx.timestamp) tx.timestamp = arkjs.slots.getTime();
+            // First Signature Signing
+            if (!multi && !second) {
+                tx.senderPublicKey = keys.publicKey;
+                tx.senderId = crypto.getAddress(keys.publicKey);
+                if (!tx.recipientId) tx.recipientId = tx.senderId;
+                tx.timestamp = arkjs.slots.getTime();
+                tx.signature = keys.sign(crypto.getHash(tx, true, true)).toDER().toString("hex");
+            }
+            // Second Signature Signing
+            if (!multi && second) {
+                tx.signSignature = keys.sign(crypto.getHash(tx, true, true)).toDER().toString("hex");
+            }
+            // Multisig Signing
+            if (multi) {
+                if (!tx.signatures) tx.signatures = [];
+                tx.signatures = tx.signatures.concat([keys.sign(crypto.getHash(tx, true, true)).toDER().toString("hex")])
+            }
+            tx.id = crypto.getId(tx);
+            if (multi) tx.ready = true;
+            console.log("Transaction signed!")
+            fs.writeFileSync('./tx.json', JSON.stringify(tx, null, 4));
+        }
+    } else {
+        console.log("No transaction found.")
+    }
+}
+
+const send = (recipient, amount) => {
+    tx = {
+        "type": 0,
+        "amount": amount,
+        "fee": 10000000,
+        "recipientId": recipient
+    }
+    fs.writeFileSync('./tx.json', JSON.stringify(tx, null, 4));
+}
+
+const vote = (publicKey, voteType) => {
+    tx = {
+        "type": 1,
+        "amount": 0,
+        "fee": 100000000,
+        "asset": { "votes": [`${publicKey}`] }
+    }
+    fs.writeFileSync('./tx.json', JSON.stringify(tx, null, 4));
+}
+
+const create = (group) => {
+    let min = group[0];
+    group = group.slice(1);
+    if (min > 1 && min <= 16 && min <= group.length) {
+        tx = {
+            "type": 4,
+            "amount": 0,
+            "fee": 500000000 + group.length * 500000000,
+            "asset": {
+                "multisignature": {
+                    "min": parseInt(min),
+                    "keysgroup": group.map(key => `+${key}`),
+                    "lifetime": 24
+                }
+            }
+        }
+        fs.writeFileSync('./tx.json', JSON.stringify(tx, null, 4));
+    } else {
+        console.log("Minimum signatures must be greater than 1 and less than 17, and not exceed keys length.")
+    }
 }
 
 switch (process.argv[2]) {
     case "key":
-        console.log("Passphrase:", pass)
-        console.log("Public key:", crypto.getKeys(pass).publicKey);
+        console.log("Passphrase:", pass, "\nPublic key:", crypto.getKeys(pass).publicKey);
         break;
     case "signmulti":
         sign(true);
@@ -29,5 +94,14 @@ switch (process.argv[2]) {
         break;
     case "signsecond":
         sign(false, true);
+        break;
+    case "send":
+        send(process.argv[3], process.argv[4]);
+        break;
+    case "vote":
+        vote(process.argv[3]);
+        break;
+    case "makemulti":
+        create(pass.split(" "))
         break;
 }
